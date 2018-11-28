ab40b5ca47677be10f5c8eb2e4e598109a562d7f
TirolJPN <tirol.jpn@gmail.com>
2018-11-28 17:29:07 +0900

    add an script

--- scripts/create_input_files_for_ngweight.py
+++ scripts/create_input_files_for_ngweight.py
@@ -13,20 +13,31 @@ cnx = cn.connect(
     user='kosuke',
     password='khkh5813',
     port='3306',
-    database='codeforces')
+    database='codeforces'
 )
 cur = cnx.cursor(buffered=True, dictionary=True)
+
+
+"""
+problem_idを引数として、ngweight用の1つの入力ファイルを作成する
+"""
+def create_an_file_for_ngweight(problem_id):
+    
 
 
 """
 ngweightに入力するファイルを用意する問題のid一覧を返す
 """
 def get_problem_list():
-
+    global cur
+    sql = r'SELECT * FROM Problem WHERE competition_id = 733 OR competition_id = 633;';
+    cur.execute(sql)
+    return cur.fetchall()
 
 
 def main():
-    
+    for problem in get_problem_list():
+        create_an_file_for_ngweight(problem['problem_id'])
 
 if __name__ == '__main__':
     main()
