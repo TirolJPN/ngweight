3b8c2c746f750d43031f26e13c07448dfa3aeb3d
TirolJPN <tirol.jpn@gmail.com>
2018-12-03 15:33:42 +0900

    add some changes

--- scripts/create_io_files_for_ngweight.py
+++ scripts/create_io_files_for_ngweight.py
@@ -99,54 +99,48 @@ def filter_cpp_comment(text):
     return ''.join(result)
 
 
-
-
-
-
-
-
 """
 problem_idを引数として、それに該当するソースコードの一覧をFileテーブルから取ってくる
 コンテスト中、コンテスト外の全てのソースコードにおいて、
 verdictがOKとなっているソースコードのみ対象
 """
 def get_right_answers(problem_id):
-        global cur
-        lang_list = ['GNU C++14', 'GNU C++11', 'GNU C++']
-        lang_select = '(%s)' % ' or '.join(["lang='%s'" % lang for lang in lang_list])
-        sql = "SELECT * FROM File WHERE problem_id LIKE \"%s\" AND verdict = \"OK\" AND %s" % (problem_id, lang_select)
-        cur.execute(sql)
-        return cur.fetchall()
+    global cur
+    lang_list = ['GNU C++14', 'GNU C++11', 'GNU C++']
+    lang_select = '(%s)' % ' or '.join(["lang='%s'" % lang for lang in lang_list])
+    sql = "SELECT * FROM File WHERE problem_id LIKE \"%s\" AND verdict = \"OK\" AND %s" % (problem_id, lang_select)
+    cur.execute(sql)
+    return cur.fetchall()
 
 """
 problem_idを引数として、ngweight用の1つの入力ファイルを作成する
 """
 def create_an_file_for_ngweight(problem_id):
-        path_file = path_feature_word_vectors + problem_id + '.txt'
+    path_file = path_feature_word_vectors + problem_id + '.txt'
 
-        # 入力ファイルがまだ作成されていなければ実行する
-        if not os.path.isfile(path_file):
-                answers = get_right_answers(problem_id)
-                with open(path_file, mode='w', encoding='utf-8') as f_ngweight:
-                        for answer in answers:
-                                # 各解答のソースコードをngweight用のテキストファイルに追記していく
-                                path_answer = path_src +  answer['file_name']
-                                with open(path_answer, mode='r', encoding='utf-8') as f_src:
-                                        code_content = f_src.read()
-                                # ヘッダーにファイル名を入れておく
-                                f_ngweight.write(chr(2))
-                                f_ngweight.write(answer['file_name'])
-                                f_ngweight.write(chr(3))
-                                s = filter_cpp_comment(code_content)
-                                # 特殊記号をとりあえず空白に変換する
-                                processed_s = re.sub("\!|\?|\"|\'|#|\$|%|&|\||\(|\)|\{|\}|\[|\]|=|<|>|\+|-|\*|\/|\\|\~|\^|@|:|;|,|\.|\s+", " ", s)
-                                # 上記置換だと、改行・スペースのスペース変換による連続スペースが残るので、それらも一つのスペースにする
-                                processed_s = re.sub(r" +", " ", processed_s)
-                                f_ngweight.write(processed_s)
-                # pythonスクリプトからngweightを直接実行し、出力ファイルを得る
-                cmd = "../bin/default/ngweight -w -s 0 < ../input_files/%s.txt > ../output_files/%s_output" % (problem_id, problem_id)
-                res = subprocess.run([cmd], stdout=subprocess.PIPE, shell=True)
-                sys.stdout.buffer.write(res.stdout)
+    # 入力ファイルがまだ作成されていなければ実行する
+    if not os.path.isfile(path_file):
+        answers = get_right_answers(problem_id)
+        with open(path_file, mode='w', encoding='utf-8') as f_ngweight:
+            for answer in answers:
+                # 各解答のソースコードをngweight用のテキストファイルに追記していく
+                path_answer = path_src +  answer['file_name']
+                with open(path_answer, mode='r', encoding='utf-8') as f_src:
+                        code_content = f_src.read()
+                # ヘッダーにファイル名を入れておく
+                f_ngweight.write(chr(2))
+                f_ngweight.write(answer['file_name'])
+                f_ngweight.write(chr(3))
+                s = filter_cpp_comment(code_content)
+                # 特殊記号をとりあえず空白に変換する
+                processed_s = re.sub("\!|\?|\"|\'|#|\$|%|&|\||\(|\)|\{|\}|\[|\]|=|<|>|\+|-|\*|\/|\\|\~|\^|@|:|;|,|\.|\s+", " ", s)
+                # 上記置換だと、改行・スペースのスペース変換による連続スペースが残るので、それらも一つのスペースにする
+                processed_s = re.sub(r" +", " ", processed_s)
+                f_ngweight.write(processed_s)
+        # pythonスクリプトからngweightを直接実行し、出力ファイルを得る
+        cmd = "../bin/default/ngweight -w -s 0 < ../input_files/%s.txt > ../output_files/%s_output" % (problem_id, problem_id)
+        res = subprocess.run([cmd], stdout=subprocess.PIPE, shell=True)
+        sys.stdout.buffer.write(res.stdout)
 
 
 
@@ -154,15 +148,15 @@ def create_an_file_for_ngweight(problem_id):
 ngweightに入力するファイルを用意する問題のid一覧を返す
 """
 def get_problem_list():
-        global cur
-        sql = r'SELECT * FROM Problem WHERE competition_id = 733 OR competition_id = 633;';
-        cur.execute(sql)
-        return cur.fetchall()
+    global cur
+    sql = r'SELECT * FROM Problem WHERE competition_id = 733 OR competition_id = 633;';
+    cur.execute(sql)
+    return cur.fetchall()
 
 
 def main():
-        for problem in get_problem_list():     
-                create_an_file_for_ngweight(problem['problem_id'])
+    for problem in get_problem_list():     
+        create_an_file_for_ngweight(problem['problem_id'])
 
 if __name__ == '__main__':
-        main()
+    main()
--- scripts/create_vectors_of_ngram.py
+++ scripts/create_vectors_of_ngram.py
@@ -6,6 +6,12 @@ ngweightでで作成した特徴語のリストから、そのN-gramの種類の
 pythonにおける階層クラスタリングは次を参照 https://qiita.com/suecharo/items/20bad5f0bb2079257568
 問題の種類によっては次元数がアホみたいなことになるが、気にしないでとりあえず実行する
 クラスタリング手法には最長距離法、メトリクスにはコサイン類似度(コサイン距離？)を用いる
+
+ある一つの問題ごとに以下を実行する
+1. 入力ファイル(input_files/hoge.txt)を参照する。1解答ごとに
+    0x02[file_name]0x03 {単語群} 0x02[file_name]0x03 {単語群} 0x02[file_name]0x03 {単語群}...
+    の形式になっているので、0x02[file_name]0x03を区切りに、1解答づつの文章を取得
+2. output_filesの特徴語のリストと照らし合わせて、特徴語の種類(outputfileの行数と同じ)の次元を持つ特徴ベクトルを作成
 """
 
 import os
@@ -26,3 +32,29 @@ cnx = cn.connect(
 )
 cur = cnx.cursor(buffered=True, dictionary=True)
 
+
+"""
+problem_idを引数として、前解答の特徴ベクトルを作成する
+"""
+def create_key_word_vectors(problem_id):
+
+
+
+"""
+ngweightに入力するファイルを用意する問題のid一覧を返す
+"""
+def get_problem_list():
+    global cur
+    sql = r'SELECT * FROM Problem WHERE competition_id = 733 OR competition_id = 633;';
+    cur.execute(sql)
+    return cur.fetchall()
+
+def main():
+    for problems in get_problem_list():
+        create_key_word_vectors(problems['problem_id'])
+        
+
+
+
+if __name__ == '__main__':
+    main()
