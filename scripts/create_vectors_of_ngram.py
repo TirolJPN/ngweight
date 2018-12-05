cbf8ed218e3484fe65a0776b1e6285be0b5a5c0b
TirolJPN <tirol.jpn@gmail.com>
2018-12-05 16:09:32 +0900

    add some functions

--- scripts/create_vectors_of_ngram.py
+++ scripts/create_vectors_of_ngram.py
@@ -23,6 +23,7 @@ from enum import  Enum
 import re
 import subprocess
 import sys
+import csv
 
 # グローバル変数
 cnx = cn.connect(
@@ -37,19 +38,34 @@ cur = cnx.cursor(buffered=True, dictionary=True)
 path_src = r'./../../Database/src_original/src_original/'
 path_input_files = r'./../input_files/'
 path_output_files = r'./../output_files/'
+path_vector_files = r'./../vector_files/'
 
 
 """
 problem_idを引数として、output_filesの特徴語一覧を取得してくる
-どの形式で返そうかな
+どの形式で返すか
 """
 def get_keywords_list(problem_id):
     path_output_file = path_output_files + problem_id + '_output'
     # ファイルが存在すれば、N-gramの一覧を取得する
     if os.path.isfile(path_output_file):
-        with open(path_output_file, mode='w', encoding='utf-8') as f_output_file:
+        list = []
+        with open(path_output_file, mode='r', encoding='utf-8') as f_output_file:
             # output_file から一行ずつ取得する
             keyword_row = f_output_file.readline()
+            while keyword_row:
+                keyword = keyword_row.split(chr(9))[5].strip()
+                # keywordがN-gramに該当
+                list.append(keyword)
+                keyword_row = f_output_file.readline()
+        return list
+    else:
+        return False
+
+
+"""
+problem_idを引数にして
+"""
             
 
 """
@@ -57,8 +73,22 @@ problem_idを引数として、前解答の特徴ベクトルの情報を持つC
 """
 def create_key_word_vectors(problem_id):
     global cur
-    # まず、特徴語一覧のファイルから、特徴語の一覧を取得してくる(そのままループを回す?)
-    get_keywords_list(problem_id)
+    keywords_list = get_keywords_list(problem_id)
+
+    # N-gram一覧の取得に成功したら
+    vector_csv_path = path_vector_files + problem_id + '.csv'
+    if (keywords_list != False):
+        with open(vector_csv_path, 'w', encoding='utf-8') as f_csv_file:
+            # 1行目に各N-Gramの項目を入れておく
+            writer = csv.writer(f_csv_file, lineterminator='\n')
+            keywords_list.insert(0, 'submission_id')
+            writer.writerow(keywords_list)
+
+            # 各提出毎にN-Gramをカウントし、csvに入れる
+
+
+
+
 
 
 """
